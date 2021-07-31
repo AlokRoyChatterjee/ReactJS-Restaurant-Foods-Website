86d745d23b09885a594815ec2e69afa6b0ca5bbe
Alok Roy Chatterjee <88044679+AlokRoyChatterjee@users.noreply.github.com>
2021-07-31 18:43:04 -0400

    Update foodimage.js

--- foodimage.js
+++ foodimage.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
 
 class foodimage extends React.Component {
 	render(){
-		return(<Imagestyle src={this.props.source} alt={this.props.text} />)
+		return(<img src={this.props.source} alt={this.props.text} />)
 	}
 }
-const imagestyle = styled.img`
-	width: 150px;
-	height: 150px;
-`;
 
 export default foodimage;
