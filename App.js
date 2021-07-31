b370e754b904c5de93221a50c3cb941e7bf8d5eb
Alok Roy Chatterjee <88044679+AlokRoyChatterjee@users.noreply.github.com>
2021-07-31 18:41:07 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,42 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import axios from 'axios';
-import styled from 'styled-components';
-import RecipeCard from './components/RecipeCard';
+import fooditems from './components/RecipeCard';
 import './App.css';
-
-export default class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       fooditem: {}
     }
   }
-
   componentDidMount(){
     const api = 'https://www.themealdb.com/api/json/v1/1/latest.php';
-    axios.get(api)
-      .then(res => {
+    axios.get(api).then(res => {
         const fooditem = res.data.meals;
         if(typeof fooditem === 'object'){
           this.setState({ fooditem });
         }
       })
-      .catch(err => {
-        console.log(err);
-      });
-
+      .catch(err => {console.log(err);});
   }
-
   render() {
     var data = this.state.fooditem;
     return (
       <appstyles className="App">
-        {data.length > 0 && <RecipeCard meals={data} />}
+        <fooditems meals={data} />
       </appstyles>
     );
   }
 }
 
-const appstyles = styled.div`
-  background-color:aqua;
-`
+export default App;
